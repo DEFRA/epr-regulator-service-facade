fde158faccc3507ad03e65047933a071ace1e564
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-08-01 12:27:53 +0100

    SQ.9

--- src/EPR.RegulatorService.Facade.Core/Services/Producer/ProducerService.cs
+++ src/EPR.RegulatorService.Facade.Core/Services/Producer/ProducerService.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Net.Http.Json;
 using EPR.RegulatorService.Facade.Core.Configs;
 using EPR.RegulatorService.Facade.Core.Models;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -11,6 +13,8 @@ public class ProducerService : IProducerService
     private readonly HttpClient _httpClient;
     private readonly ILogger<ProducerService> _logger;
     private readonly AccountsServiceApiConfig _config;
+
+    private readonly string[] allowedSchemes = { "https" };
 
     public ProducerService(
         HttpClient httpClient,
@@ -26,18 +30,32 @@ public class ProducerService : IProducerService
     {
         var url = string.Format($"{_config.Endpoints.GetOrganisationsBySearchTerm}", userId, currentPage, pageSize, searchTerm);
 
+        Uri uri = new Uri(url);
+
         _logger.LogInformation("Attempting to fetch organisations by searchTerm '{searchTerm}'", searchTerm);
 
-        return await _httpClient.GetAsync(url);
+        if (allowedSchemes.Contains(uri.Scheme))
+        {
+            return await _httpClient.GetAsync(url);
+        }
+
+        return null;
     }
     
     public async Task<HttpResponseMessage> GetOrganisationDetails(Guid userId, Guid externalId)
     {
         var url = string.Format($"{_config.Endpoints.GetOrganisationDetails}", userId, externalId);
 
+        Uri uri = new Uri(url);
+
         _logger.LogInformation("Attempting to fetch organisation details for organisation'{externalId}'", externalId);
 
-        return await _httpClient.GetAsync(url);
+        if (allowedSchemes.Contains(uri.Scheme))
+        {
+            return await _httpClient.GetAsync(url);
+        }
+
+        return null;
     }
     public async Task<HttpResponseMessage> RemoveApprovedUser(RemoveApprovedUsersRequest model)
     {
