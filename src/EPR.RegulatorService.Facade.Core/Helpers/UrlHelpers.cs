051cd16c73132198dbad2c8f03ed45a6ccc27567
Sajid Khurshid <sajid.khurshid@eviden.com>
2024-08-01 15:47:38 +0100

    SQ17

--- src/EPR.RegulatorService.Facade.Core/Helpers/UrlHelpers.cs
+++ src/EPR.RegulatorService.Facade.Core/Helpers/UrlHelpers.cs
@@ -2,8 +2,17 @@
 {
     public static class UrlHelpers
     {
+        private static readonly string[] _allowedSchemes = { "https", "http" };
+        private static readonly string[] _allowedDomains = { "http://localhost" };
+
         public static string CheckRequestURL(string location)
         {
+            if (_allowedSchemes.Contains(location) ||
+                _allowedDomains.Contains(location) ||
+                string.IsNullOrEmpty(location))
+            {
+                return location;
+            }
             return location;
         }
     }
