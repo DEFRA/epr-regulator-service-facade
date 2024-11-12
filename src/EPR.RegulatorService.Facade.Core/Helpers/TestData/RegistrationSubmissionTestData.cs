8cc2619bf5ced2c0be58515ae2bebf9b3501cf1a
Karthik K V <karthik.vishwanathan.external@eviden.com>
2024-11-12 10:43:48 +0000

    check culture issue after deploying into linux container

--- src/EPR.RegulatorService.Facade.Core/Helpers/TestData/RegistrationSubmissionTestData.cs
+++ src/EPR.RegulatorService.Facade.Core/Helpers/TestData/RegistrationSubmissionTestData.cs
@@ -36,7 +36,7 @@ namespace EPR.RegulatorService.Facade.Core.Helpers.TestData;
             {
                 var fields = line.Split('\t');
 
-                var dateTime = DateTime.Parse(fields[8], new CultureInfo("en-GB"));
+                var dateTime = DateTime.Parse(fields[8], CultureInfo.CurrentCulture);
                 objRet.Add(new RegistrationSubmissionOrganisationDetails
                 {
                     OrganisationReference = fields[0][..10],
