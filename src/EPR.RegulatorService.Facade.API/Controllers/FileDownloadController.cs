2c0156e95d0b7cdd7cde68b1b80ecc2e9522377c
Idris Abu <idris.abu@eviden.com>
2025-01-13 17:20:00 +0000

    Changes to facilitate testing scenarios 6 and 7

--- src/EPR.RegulatorService.Facade.API/Controllers/FileDownloadController.cs
+++ src/EPR.RegulatorService.Facade.API/Controllers/FileDownloadController.cs
@@ -33,44 +33,7 @@ public class FileDownloadController : ControllerBase
     [Route("file-download")]
     public async Task<IActionResult> DownloadFile([FromBody] FileDownloadRequest request)
     {
-        // Get file from blob storage
-        var stream = await _blobStorageService.DownloadFileStreamAsync(request.SubmissionType, request.BlobName);
 
-        // send FileDownloadRequest to Trade antivirus API for checking
-        var userId = User.UserId();
-        var email = User.Email();
-        var truncatedFileName = FileHelpers.GetTruncatedFileName(request.FileName, FileConstants.FileNameTruncationLength);
-        var antiVirusResponse = await _antivirusService.SendFile(request.SubmissionType, request.FileId, truncatedFileName, stream, userId, email);
-        var antiVirusResult = await antiVirusResponse.Content.ReadAsStringAsync();
-
-        // Create a new submissions event for the download attempt
-        var fileDownloadCheckEvent = new FileDownloadCheckEvent()
-        {
-            UserEmail = email,
-            ContentScan = antiVirusResult,
-            FileId = request.FileId,
-            FileName = request.FileName,
-            BlobName = request.BlobName,
-            SubmissionType = request.SubmissionType
-        };
-
-        var submissionEvent = await _submissionService.CreateSubmissionEvent(request.SubmissionId, fileDownloadCheckEvent, User.UserId());
-        if (!submissionEvent.IsSuccessStatusCode)
-        {
-            return new BadRequestObjectResult("There was an error communicating with the submissions API.");
-        }
-
-        // if clean, return file
-        if (antiVirusResult == ContentScan.Clean)
-        {
-            var file = new FileContentResult(stream.ToArray(), "text/csv")
-            {
-                FileDownloadName = request.FileName
-            };
-
-            return file;
-        }
-
-        return new ObjectResult("The file was found but it was flagged as infected. It will not be downloaded.") {  StatusCode = StatusCodes.Status403Forbidden };
+        return new ObjectResult("The file was found but it was flagged as infected. It will not be downloaded.") {  StatusCode = StatusCodes.Status500InternalServerError };
     }
 }
