b9b2605631151b8bc290fd789f5ad1190be81700
Shehzad Ismail <85490459+shehzad-ismail8@users.noreply.github.com>
2025-02-18 16:40:32 +0000

    [feature/496616: Resubmission: Send email to Producer for accept decision (English)] (#65)
    * [feature/496616-resubmission-accept-email: WIP English resubmission decision email]
    
    * [feature/496616-resubmission-accept-email: Removed Welsh resubmission decision template]
    
    * [feature/496616-resubmission-accept-email: Added logic to account for resubmission emails for accept and reject]
    
    * [feature/496616-resubmission-accept-email: Added template ID for new email template]
    
    * [feature/496616-resubmission-accept-email: Wrote unit tests for SendEventEmail]
    
    * [feature/496616-resubmission-accept-email: Added another unit test]
    
    * [feature/499812: Resubmission - send email to Producer for accept decision (Welsh)] (#66)
    
    * [feature/499812-resubmission-decision-email-welsh: Added the welsh translation email template and logic]
    
    * [feature/499812-resubmission-decision-email-welsh: Added unit tetsts]
    
    * [feature/499812-resubmission-decision-email-welsh: Removed unnecessary unit tests and refactored messaging service code]
    
    * [feature/499812-resubmission-decision-email-welsh: Added unit tests]

--- src/EPR.RegulatorService.Facade.API/Controllers/OrganisationRegistrationSubmissionsController.cs
+++ src/EPR.RegulatorService.Facade.API/Controllers/OrganisationRegistrationSubmissionsController.cs
@@ -205,8 +205,15 @@ public class OrganisationRegistrationSubmissionsController(
             switch (request.Status)
             {
                 case Core.Enums.RegistrationSubmissionStatus.Refused:
-                case Core.Enums.RegistrationSubmissionStatus.Granted:
-                    messagingService.OrganisationRegistrationSubmissionDecision(model); //Send same email
+                case Core.Enums.RegistrationSubmissionStatus.Granted: // Same email for both accept and reject
+                    if (request.IsResubmission)
+                    {
+                        messagingService.OrganisationRegistrationResubmissionDecision(model);
+                    }
+                    else
+                    {
+                        messagingService.OrganisationRegistrationSubmissionDecision(model);
+                    }
                     break;
                 case Core.Enums.RegistrationSubmissionStatus.Queried:
                     messagingService.OrganisationRegistrationSubmissionQueried(model);
