2e5893f4ecf46bea77ed982e334ab18232cc83cd
Zozan KIRKUTAY <167984276+zozanKirkutay@users.noreply.github.com>
2025-03-28 11:40:37 +0000

    feature/526077 Setup connection between Regulator Facade and PRN Back… (#83)
    * feature/526077 Setup connection between Regulator Facade and PRN Backend Service
    
    * 526077 Setup connection between Regulator Facade and PRN Backend Service
    
    * feature/526077 Setup connection between Regulator Facade and PRN Backend Service
    
    * feature/526077 Setup connection between Regulator Facade and PRN Backend Service
    
    * feature/526077 Setup connection between Regulator Facade and PRN Backend Service
    
    * feature/526077 Setup connection between Regulator Facade and PRN Backend Service

---
+++ src/EPR.RegulatorService.Facade.Core/Clients/PrnBackendServiceClient/PrnBackendServiceClient.cs
@@ -0,0 +1,16 @@
+﻿using EPR.RegulatorService.Facade.Core.Configs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EPR.RegulatorService.Facade.Core.Clients.PrnBackendServiceClient;
+
+[ExcludeFromCodeCoverage]
+public class PrnBackendServiceClient(
+HttpClient httpClient,
+IOptions<PrnBackendServiceApiConfig> options,
+ILogger<PrnBackendServiceClient> logger)
+: IPrnBackendServiceClient
+{
+    private readonly PrnBackendServiceApiConfig _config = options.Value;
+}
