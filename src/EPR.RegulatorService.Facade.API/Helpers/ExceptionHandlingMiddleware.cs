6349849c6c25147b782f530c8819ed0470e191fe
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-04-03 12:29:32 +0100

    feature/523464 update registration outcome

--- src/EPR.RegulatorService.Facade.API/Helpers/ExceptionHandlingMiddleware.cs
+++ src/EPR.RegulatorService.Facade.API/Helpers/ExceptionHandlingMiddleware.cs
@@ -17,33 +17,11 @@ public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<Exception
         {
             await next(httpContext);
         }
-        catch (HttpRequestException httpRequestException)
-        {
-            logger.LogError(httpRequestException, "An HTTP request exception occurred.");
-            await HandleHttpRequestExceptionAsync(httpContext, httpRequestException);
-        }
         catch (Exception ex)
         {
             logger.LogError(ex, "An unhandled exception occurred.");
             await HandleExceptionAsync(httpContext, ex);
         }
-    }
-
-    private static async Task HandleHttpRequestExceptionAsync(HttpContext context, HttpRequestException ex)
-    {
-        var statusCode = (int)(ex.StatusCode ?? HttpStatusCode.InternalServerError);
-
-        context.Response.StatusCode = statusCode;
-        context.Response.ContentType = "application/json";
-
-        var errorResponse = new
-        {
-            title = "An HTTP request error occurred.",
-            status = statusCode,
-            detail = ex.InnerException?.Message
-        };
-
-        await context.Response.WriteAsJsonAsync(errorResponse);
     }
 
     private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
@@ -60,11 +38,12 @@ public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<Exception
 
         var errorResponse = new
         {
-            title = "An error occurred while processing your request",
+            title = ex is HttpRequestException ? "An HTTP request error occurred." : "An error occurred while processing your request",
             status = statusCode,
             detail = ex?.InnerException?.Message
         };
 
         await context.Response.WriteAsJsonAsync(errorResponse);
     }
+
 }
