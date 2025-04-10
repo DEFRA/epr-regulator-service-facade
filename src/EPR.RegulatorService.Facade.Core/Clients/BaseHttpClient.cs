f67caead67353b5ec6d0cf9c6e3807fdda8c8cea
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-04-10 09:01:39 +0100

    feature/523464 update registration outcome

--- src/EPR.RegulatorService.Facade.Core/Clients/BaseHttpClient.cs
+++ src/EPR.RegulatorService.Facade.Core/Clients/BaseHttpClient.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
-using System.Reflection;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using EPR.RegulatorService.Facade.Core.Constants;
-using EPR.RegulatorService.Facade.Core.Helpers.Converter;
 
 namespace EPR.RegulatorService.Facade.Core.Clients;
 
@@ -24,22 +22,6 @@ public abstract class BaseHttpClient
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             WriteIndented = true
         };
-
-        RegisterEnumConverters();
-    }
-
-    private void RegisterEnumConverters()
-    {
-        var enumTypes = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => t.IsEnum || (Nullable.GetUnderlyingType(t) != null && Nullable.GetUnderlyingType(t).IsEnum))
-            .ToList();
-
-        foreach (var enumType in enumTypes)
-        {
-            var converterType = typeof(CustomEnumConverter<>).MakeGenericType(enumType);
-            var converter = (JsonConverter)Activator.CreateInstance(converterType);
-            _jsonOptions.Converters.Add(converter);
-        }
     }
 
     protected async Task<TResponse> GetAsync<TResponse>(string url)
@@ -47,8 +29,11 @@ public abstract class BaseHttpClient
         var response = await _httpClient.GetAsync(url);
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<TResponse>(json, _jsonOptions);
+        return await response.Content.ReadFromJsonAsync<TResponse>(new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+        });
     }
 
     protected async Task<TResponse> PostAsync<TRequest, TResponse>(string url, TRequest data)
@@ -57,8 +42,11 @@ public abstract class BaseHttpClient
         var response = await _httpClient.PostAsync(url, content);
         response.EnsureSuccessStatusCode();
 
-        var json = await response.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<TResponse>(json, _jsonOptions);
+        return await response.Content.ReadFromJsonAsync<TResponse>(new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+        });
     }
 
     protected async Task<TResponse> PutAsync<TRequest, TResponse>(string url, TRequest data)
