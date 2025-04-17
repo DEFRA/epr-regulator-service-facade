ba668ff9d8176490a1a7f7a723e0706a83f28e09
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-04-17 10:05:02 +0100

    minor change

--- src/EPR.RegulatorService.Facade.Core/Models/ReprocessorExporter/Registrations/UpdateRegulatorTaskBaseDto.cs
+++ src/EPR.RegulatorService.Facade.Core/Models/ReprocessorExporter/Registrations/UpdateRegulatorTaskBaseDto.cs
@@ -4,10 +4,11 @@ using System.ComponentModel.DataAnnotations;
 namespace EPR.RegulatorService.Facade.Core.Models.ReprocessorExporter.Registrations;
 public abstract class UpdateRegulatorTaskBaseDto
 {
+    [Required]
     public required string TaskName { get; set; }
     [Required]
     public required RegistrationTaskStatus Status { get; set; }
     [MaxLength(500)]
     public string? Comments { get; set; } = string.Empty;
-    public required string UserName { get; set; }
+    public string UserName { get; set; }
 }
