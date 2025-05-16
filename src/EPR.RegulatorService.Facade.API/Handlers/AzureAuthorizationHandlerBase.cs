83322060897d5b18b23da96253b251390c742bb1
Zozan Kirkutay <zozan.kirkutay.external@eviden.com>
2025-05-16 10:19:04 +0100

    fixed dublication error

---
+++ src/EPR.RegulatorService.Facade.API/Handlers/AzureAuthorizationHandlerBase.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Core;
+using System.Net.Http.Headers;
+
+namespace EPR.RegulatorService.Facade.API.Handlers;
+
+public abstract class AzureAuthorizationHandlerBase : DelegatingHandler
+{
+    private readonly TokenRequestContext _tokenRequestContext;
+    private readonly TokenCredential _credential;
+    private readonly string _scheme;
+
+    protected AzureAuthorizationHandlerBase(string serviceClientId, TokenCredential credential, string scheme = Microsoft.Identity.Web.Constants.Bearer)
+    {
+        if (string.IsNullOrEmpty(serviceClientId))
+        {
+            return;
+        }
+
+        _tokenRequestContext = new TokenRequestContext(new[] { serviceClientId });
+        _credential = credential;
+        _scheme = scheme;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var tokenResult = await _credential.GetTokenAsync(_tokenRequestContext, cancellationToken);
+        request.Headers.Authorization = new AuthenticationHeaderValue(_scheme, tokenResult.Token);
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
