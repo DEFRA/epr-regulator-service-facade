db7cdab0a81a06a7152c12531dcb12ef3a0bf888
adrianc-eviden <adrian.clare.external@eviden.com>
2025-05-20 15:18:18 +0100

    Add file download endpoint for registrations (#97)
    * add download file for registrations
    
    * Use config for download file
    
    * de-couple code from persist flag
    
    * remove file from gitignore
    
    * remove unused method
    
    * update file download
    
    * remove tostring reference
    
    * re-factor to fileId

---
+++ src/EPR.RegulatorService.Facade.Core/Models/ReprocessorExporter/Registrations/FileDownloadRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using EPR.RegulatorService.Facade.Core.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EPR.RegulatorService.Facade.Core.Models.ReprocessorExporter.Registrations
+{
+    public class FileDownloadRequestDto
+    {
+        [Required]
+        public Guid FileId { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+    }
+}
