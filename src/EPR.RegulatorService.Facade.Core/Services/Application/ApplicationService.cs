6c0d9d52bdc1a61d87bd3762a729a1b42fa921f5
Fahad Ali <fahad.ali@eviden.com>
2025-09-02 11:59:26 +0100

    code replaced because of sonarqube isue with string.format

--- src/EPR.RegulatorService.Facade.Core/Services/Application/ApplicationService.cs
+++ src/EPR.RegulatorService.Facade.Core/Services/Application/ApplicationService.cs
@@ -72,12 +72,11 @@ public class ApplicationService : IApplicationService
     private static string BuildPendingApplicationsUrl(
     string endpoint, Guid userId, int currentPage, int pageSize, string? organisationName, string? applicationType)
     {
-        return string.Format(CultureInfo.InvariantCulture, 
-            endpoint,
-            Uri.EscapeDataString(userId.ToString()),
-            Uri.EscapeDataString(currentPage.ToString(CultureInfo.InvariantCulture)),
-            Uri.EscapeDataString(pageSize.ToString(CultureInfo.InvariantCulture)),
-            Uri.EscapeDataString(organisationName ?? string.Empty),
-            Uri.EscapeDataString(applicationType ?? string.Empty));
+         return endpoint
+        .Replace("{0}", Uri.EscapeDataString(userId.ToString()), StringComparison.Ordinal)
+        .Replace("{1}", Uri.EscapeDataString(currentPage.ToString(CultureInfo.InvariantCulture)), StringComparison.Ordinal)
+        .Replace("{2}", Uri.EscapeDataString(pageSize.ToString(CultureInfo.InvariantCulture)), StringComparison.Ordinal)
+        .Replace("{3}", Uri.EscapeDataString(organisationName ?? string.Empty), StringComparison.Ordinal)
+        .Replace("{4}", Uri.EscapeDataString(applicationType ?? string.Empty), StringComparison.Ordinal);
     }
 }
