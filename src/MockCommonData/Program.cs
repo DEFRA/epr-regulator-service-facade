e2d6721ea76b9b0ada281b4a3f3ce343c1aadff9
Tim Abell <t@0x5.uk>
2026-02-03 16:05:10 +0000

    Run mock common-data-api on https
    To match existing local running of common-data-api service

--- src/MockCommonData/Program.cs
+++ src/MockCommonData/Program.cs
@@ -9,7 +9,7 @@ public static class Program
     {
         const int port = 5001; // same as common-data-api when run locally, to make it easy to flip between them
         Console.WriteLine($"MockCommonData starting on http://localhost:{port}");
-        MockCommonDataServer.Start(port: port, useSsl: false);
+        MockCommonDataServer.Start(port: port, useSsl: true);
 
         Console.WriteLine("Press any key to stop.");
         Console.ReadKey();
