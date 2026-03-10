d269d56ddfc9ee82fa6e7fd7bb2c9001ccaa0ce5
Tim Abell <t@0x5.uk>
2026-03-10 13:03:55 +0000

    Sonarqube

--- src/EPR.RegulatorService.Facade.API/HealthChecks/HealthCheckOptionBuilder.cs
+++ src/EPR.RegulatorService.Facade.API/HealthChecks/HealthCheckOptionBuilder.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace EPR.RegulatorService.Facade.API.HealthChecks;
 
+[ExcludeFromCodeCoverage]
 public static class HealthCheckOptionBuilder
 {
     public static HealthCheckOptions Build(string? buildNumber = null, string? gitSha = null) => new()
